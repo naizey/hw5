54e9e73a89b1b67142b7fcec72a0a0c47144eb83
Nairi Zeytounzian <nzeytounzian@gmail.com>
2025-04-22 22:52:56 -0700

    trying the optimization code again...last one didn't work

--- wordle.cpp
+++ wordle.cpp
@@ -23,39 +23,63 @@ set<string> wordle(const string& in, const string& floating, const set<string>& 
 
 void findWord(string current, int index, string floating, const set<string>& dict, set<string>& results)
 {
-    // Base case: word is fully built
-    if (index == current.size()) 
+    //base case - word has all letters found (floating letters are used)
+    if(index == current.size()) 
     {
-        if (floating.empty() && dict.find(current) != dict.end()) 
+        //if all the floating letters are used and if current is a valid word
+        if(floating.empty() && dict.find(current) != dict.end()) 
         {
+            //add to results
             results.insert(current);
         }
         return;
     }
 
-    // If fixed letter, skip to next
-    if (current[index] != '-') 
+    //if the letter is fixed, doesn't have a dash
+    if(current[index] != '-') 
     {
+        //go to next letter
         findWord(current, index + 1, floating, dict, results);
         return;
     }
 
-    // Try placing floating letters first
-    for (size_t i = 0; i < floating.size(); ++i) 
+    //keep track of dashes left till end
+    int dashes_left = 0;
+    for(size_t i = index; i < current.size(); ++i)
+    {
+        //increase dash count if on a dash "letter"
+        if(current[i] == '-')
+        {
+            dashes_left++;
+        }
+    }
+
+    if((int)floating.size() > dashes_left)
+    {
+        return;
+    }
+
+    //guess dash letters by putting in floating letters 
+    for(size_t i = 0; i < floating.size(); ++i) 
     {
         char c = floating[i];
-        current[index] = c;
-        string nextFloating = floating;
-        nextFloating.erase(i, 1); // remove used floating letter
-        findWord(current, index + 1, nextFloating, dict, results);
+        current[index] = c; //set letter to the first floating
+        string next = floating; //save the floating letters
+        next.erase(i, 1); //remove the letter used in floating
+        findWord(current, index + 1, next, dict, results); //feed in next as the floating argument
     }
 
     // Try placing non-floating letters
-    for (char c = 'a'; c <= 'z'; ++c) 
+    if((int)floating.size() < dashes_left)
     {
-        if (floating.find(c) != string::npos) 
-            continue; // don't use floating again
-        current[index] = c;
-        findWord(current, index + 1, floating, dict, results);
+        for (char c = 'a'; c <= 'z'; ++c) 
+        {
+            if(floating.find(c) != string::npos) 
+            {
+                continue; //don't use floating again
+            }
+            current[index] = c;
+            findWord(current, index + 1, floating, dict, results);
+        }
     }
 }
