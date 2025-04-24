a08bd2ca6d0efd7ebcfcb59a0093bf73781ca4a2
Nairi Zeytounzian <nzeytounzian@gmail.com>
2025-04-24 12:24:51 -0700

    testing1

--- wordle.cpp
+++ wordle.cpp
@@ -17,10 +17,23 @@ using namespace std;
 set<string> wordle(const string& in, const string& floating, const set<string>& dict)
 {
     std::set<std::string> results; //init return set
-    std::string input = in; //send in a copy of string
-    std::string floats = floating; //send in a copy of floating letters
-    findWord(0, input, floats, dict, results); //pass in index, 
+    //std::string input = in; //send in a copy of string
+    //std::string floats = floating; //send in a copy of floating letters
+    findWord(0, in, floating, dict, results); //pass in index, 
     return results; 
+}
+
+int countDashes(const std::string& word, int index)
+{
+    int num = 0;
+    for(int i = index; i < word.size(); i++)
+    {
+        if(word[i] == '-')
+        {
+            ++num;
+        }
+    }
+    return num;
 }
 
 void findWord(int index, std::string input, std::string floating, const set<string>& dict, std::set<std::string>& results)
@@ -29,7 +42,7 @@ void findWord(int index, std::string input, std::string floating, const set<stri
     if(index == input.size()) 
     {
         //if all the floating letters are used and if current is a valid word
-        if(floating.length() == 0 && dict.find(input) != dict.end()) 
+        if(floating.empty() == 0 && dict.find(input) != dict.end()) 
         {
             //add to results
             results.insert(input);
@@ -42,38 +55,52 @@ void findWord(int index, std::string input, std::string floating, const set<stri
     {
         //go to next letter
         findWord(input, index + 1, floating, dict, results);
-        //return; //added a return to kick out
+        return; //added a return to kick out
     }
 
-    else 
-    {
+    // else 
+    // {
         //count the number of dashes to reduce the amount of letters guessed, no wasted checks)
-        int num_dashes = 0;
-        for(int i = 0; i < input.length(); i++)
-        {
-            //increment the number of dashes in "in"
-            num_dashes += (int)(input[i] == '-');
-        }
+        int num_dashes = countDashes(input, index);
 
         //guess dash letters by putting in floating letters 
+        std::set<char> letters_used;
         for(int i = 0; i < floating.length(); i++) 
         {
-            //try first floating letter
-            input[index] = floating[i]; //set letter to the first floating
-            //get the rest of the letters
-            std::string next = floating.substr(0, i) + floating.substr(i + 1); //save the floating letters
-            findWord(index + 1, input, next, dict, results); //feed in next as the floating argument
+            char c = floating[i];
+            if(letters_used.count(c))
+            {
+                continue;
+            }
+            letters_used.insert(c); //add to the set of letters used
+            std::string next = input;
+            next[index] = c;
+
+            // //try first floating letter
+            // input[index] = floating[i]; //set letter to the first floating
+            // //get the rest of the letters
+            std::string new_floats = floating.substr(0, i) + floating.substr(i + 1); //save the floating letters
+            findWord(index + 1, next, new_floats, dict, results); //feed in next as the floating argument
         }
 
         //condition is necessary to make efficient
         if(num_dashes >= floating.length()) //if there are more dashes than letters left to guess from...
         {
-            for(int c = (int)'a'; c <= (int)'z'; c++) //now guess through alphabet, since its not a floating letter
+            for(char c = 'a'; c <= 'z'; c++) //now guess through alphabet, since its not a floating letter
             {
-                input[index] = char(c); //guess the letter
-                findWord(index + 1, input, floating, dict, results); //recurse again
+                if(floating.find(c) != std::string::npos || letters_used.count(c)) //if the letter is a floating letter, skip it
+                {
+                    continue;
+                }
+
+                //redo same case
+                std::string next = input;
+                next[index] = c;
+
+                //input[index] = char(c); //guess the letter
+                findWord(index + 1, next, floating, dict, results); //recurse again
             }
         }
-    }
+    // }
     
 }
